8187dcdd2f93bf7caea529941218ee8cdb92111a
Mohammed Rashith KP <mohammedrashithkp@gmail.com>
2024-11-21 20:54:05 +0530

    Added a Pre-installation script and modified the script accordingly

--- scripts/install_ros2.sh
+++ scripts/install_ros2.sh
@@ -7,42 +7,7 @@ get_ubuntu_version() {
     echo $VERSION_CODENAME
 }
 
-# Function to check if the current OS version is supported for Jazzy
-check_os_version() {
-    VERSION=$(get_ubuntu_version)
 
-    # Jazzy supports only Ubuntu 24.04 (Noble)
-    if [[ "$VERSION" == "noble" ]]; then
-        ROS_DISTRO="jazzy"
-        echo "Supported OS: Ubuntu 24.04 (Noble). Proceeding with ROS 2 Jazzy installation."
-    # Humble and Iron support only Ubuntu 22.04 (Jammy)
-    elif [[ "$VERSION" == "jammy" ]]; then
-        ROS_DISTRO="humble or iron"
-        echo "Supported OS: Ubuntu 22.04 (Jammy). Proceeding with ROS 2 Humble or Iron installation."
-    else
-        echo "Unsupported OS version. This script supports only Ubuntu 22.04 (Jammy) or Ubuntu 24.04 (Noble)."
-        exit 1
-    fi
-}
-
-# Call the function to check the OS version
-check_os_version
-
-# If the OS is valid, proceed with the installation
-
-# Make sure your system is up-to-date
-echo "Updating your system..."
-sudo apt update && sudo apt upgrade -y
-
-# Install required dependencies
-echo "Installing dependencies..."
-sudo apt install curl software-properties-common locales -y
-
-# Generate locales if needed
-echo "Setting up locales..."
-sudo locale-gen en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
 
 # Add the ROS 2 repository depending on the version
 if [[ "$ROS_DISTRO" == "jazzy" ]]; then
